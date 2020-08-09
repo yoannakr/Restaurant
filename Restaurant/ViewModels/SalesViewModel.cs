c3685300e0d80ebf82f4b92fc9ddd3e84a0245dc
yoannakr <jkrmilkova@gmail.com>
2020-08-09 22:58:06 +0300

    Made SalesView and SalesViewModel.

---
+++ Restaurant/ViewModels/SalesViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Restaurant.Views;
+
+namespace Restaurant.ViewModels
+{
+    public class SalesViewModel : BaseViewModel
+    {
+        #region Declarations
+
+        private AllItemViewModel allItemViewModel;
+        private SelectedItemViewModel selectedItemViewModel;
+
+        #endregion
+
+        #region Properties
+
+        public AllItemViewModel AllItemViewModel
+        {
+            get
+            {
+                if (allItemViewModel == null)
+                {
+                    allItemViewModel = new AllItemViewModel();
+                    AllItemView allItemView = new AllItemView();
+
+                    allItemViewModel.View = allItemView;
+
+                    allItemView.DataContext = allItemViewModel;
+                }
+
+                return allItemViewModel;
+            }
+            set
+            {
+                allItemViewModel = value;
+                OnPropertyChanged("AllItemViewModel");
+            }
+        }
+
+        public SelectedItemViewModel SelectedItemViewModel
+        {
+            get
+            {
+                if (selectedItemViewModel == null)
+                {
+                    selectedItemViewModel = new SelectedItemViewModel();
+                    SelectedItemView selectedItemView = new SelectedItemView();
+
+                    allItemViewModel.View = selectedItemView;
+
+                    selectedItemView.DataContext = allItemViewModel;
+                }
+
+                return selectedItemViewModel;
+            }
+            set
+            {
+                selectedItemViewModel = value;
+                OnPropertyChanged("SelectedItemViewModel");
+            }
+        }
+
+        #endregion
+    }
+}
