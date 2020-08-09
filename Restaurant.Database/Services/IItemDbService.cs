e21fd156071beab3c0c3ec2bcc614430debb7fb2
yoannakr <jkrmilkova@gmail.com>
2020-08-09 22:53:01 +0300

    Add ItemDbService.

---
+++ Restaurant.Database/Services/IItemDbService.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using Restaurant.Database.Models;
+
+namespace Restaurant.Database.Services
+{
+    public interface IItemDbService
+    {
+        void CreateItem(string name, decimal price, byte[] imageContent);
+
+        IQueryable<Item> GetAllItems();
+    }
+}
---
+++ Restaurant.Database/Services/Implementations/ItemDbService.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using Restaurant.Database.Data;
+using Restaurant.Database.Models;
+
+namespace Restaurant.Database.Services.Implementations
+{
+    public class ItemDbService : IItemDbService
+    {
+        #region Declarations
+
+        private readonly RestaurantDbContext context;
+
+        #endregion
+
+        #region Constructor
+
+        public ItemDbService()
+        {
+            context = new RestaurantDbContext();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public IQueryable<Item> GetAllItems()
+        {
+            return context.Items;
+        }
+
+        public void CreateItem(string name, decimal price, byte[] imageContent)
+        {
+            Image image = new Image()
+            {
+                Content = imageContent
+            };
+
+            context.Images.Add(image);
+
+            context.Items.Add(new Item()
+            {
+                Name = name,
+                Price = price,
+                Image = image
+            });
+
+            context.SaveChanges();
+        }
+
+        #endregion
+    }
+}
