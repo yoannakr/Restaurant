e190fbca63818f443bd438737ff929396aca138d
yoannakr <jkrmilkova@gmail.com>
2020-08-09 22:59:20 +0300

    Made AllItemView and AllItemViewModel and get all items from database.

---
+++ Restaurant/ViewModels/AllItemViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using Restaurant.Services;
+using System.Collections.Generic;
+using Restaurant.Services.Implementations;
+
+namespace Restaurant.ViewModels
+{
+    public class AllItemViewModel : BaseViewModel
+    {
+        #region Declarations
+
+        private IItemService itemService;
+        private List<ItemViewModel> itemViewModels;
+
+        #endregion
+
+        #region Constructor
+
+        public AllItemViewModel()
+        {
+            itemService = new ItemService();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public List<ItemViewModel> ItemViewModels
+        {
+            get
+            {
+                if (itemViewModels == null)
+                    itemViewModels = new List<ItemViewModel>();
+
+                itemViewModels = itemService.GetAllItems().ToList();
+
+                return itemViewModels;
+
+            }
+        }
+
+        #endregion
+    }
+}
