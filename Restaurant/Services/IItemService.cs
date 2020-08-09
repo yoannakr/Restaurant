64dcf713e7a53fe35e36eaaad46504a02b39d377
yoannakr <jkrmilkova@gmail.com>
2020-08-09 22:54:42 +0300

    Add ItemService.

---
+++ Restaurant/Services/IItemService.cs
@@ -0,0 +1,12 @@
+﻿using Restaurant.ViewModels;
+using System.Collections.Generic;
+
+namespace Restaurant.Services
+{
+    public interface IItemService
+    {
+        void CreateItem(string name, decimal price, byte[] imageContent);
+
+        IEnumerable<ItemViewModel> GetAllItems();
+    }
+}
---
+++ Restaurant/Services/Implementations/ItemService.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Restaurant.ViewModels;
+using System.Collections.Generic;
+using Restaurant.Database.Services;
+using Restaurant.Database.Services.Implementations;
+
+namespace Restaurant.Services.Implementations
+{
+    public class ItemService : IItemService
+    {
+        #region Declarations
+
+        private readonly IItemDbService itemDb;
+
+        #endregion
+
+        #region Constructor
+
+        public ItemService()
+        {
+            itemDb = new ItemDbService();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public IEnumerable<ItemViewModel> GetAllItems()
+        {
+            return itemDb.GetAllItems().Select(i => new ItemViewModel
+            {
+                Name = i.Name,
+                Price = i.Price,
+                Image = i.Image.Content
+            }).ToList();
+        }
+
+        public void CreateItem(string name, decimal price, byte[] imageContent)
+        {
+            itemDb.CreateItem(name, price, imageContent);
+        }
+
+        #endregion
+    }
+}
